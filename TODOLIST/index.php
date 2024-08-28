52b268ef5dd7a1cf51f34c238c9939b266b4e16d
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-28 22:51:38 +0900

    08282024

---
+++ TODOLIST/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODOLIST</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+    <main>
+        <h1>TODOリスト</h1>
+
+        <div class="dateName">
+            <input type="date" name="date" id="date">
+            <input type="text" name="name" id="taskList">
+        </div>
+
+
+    </main>
+
+</body>
+</html>
