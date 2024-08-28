ad2c239745b5e8810f4840c8eaecdbe1252dc34e
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-28 23:47:16 +0900

    08282024

--- TODOLIST/index.php
+++ TODOLIST/index.php
@@ -19,7 +19,26 @@
             <button>追加</button>
         </div>
 
-
+        <table>
+            <tr>
+                <th>期限日</th>
+                <th>項目</th>
+                <th>未完了</th>
+                <th>完了</th>
+                <th>削除</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>date</td>
+                <td>project</td>
+                <td>Unfinish_radiobutton</td>
+                <td>Finished_radiobutton</td>
+                <td>delete_radiobutton</td>
+                <td>
+                    <button>実行</button>
+                </td>
+            </tr>
+        </table>
     </main>
 
 </body>
