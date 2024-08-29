e37c762a3ebc1c6cd1b3ff64b06298b58b24d5ca
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-29 23:09:04 +0900

    08282024

--- TODOLIST/index.php
+++ TODOLIST/index.php
@@ -18,22 +18,62 @@
             <input type="text" name="name" id="listName">
             <button>追加</button>
         </div>
-
-        <table>
+        
+         <table>
             <tr>
-                <th>期限日</th>
-                <th>項目</th>
-                <th>未完了</th>
-                <th>完了</th>
-                <th>削除</th>
-                <th></th>
+                <th class="date">期限日</th>
+                <th class="project">項目</th>
+                <th class="unfinish">未完了</th>
+                <th class="finish">完了</th>
+                <th class="delete" >削除</th>
+                <th class="space"></th>
             </tr>
             <tr>
-                <td>date</td>
-                <td>project</td>
-                <td>Unfinish_radiobutton</td>
-                <td>Finished_radiobutton</td>
-                <td>delete_radiobutton</td>
+                <td class="date">2020-01-01</td>
+                <td class="project">
+                    元旦なので初詣に行く
+                </td>
+                <td class="unfinish">
+                    <input type="radio" name="unfinish" value="unfinish">
+                </td>
+                <td class="finish">
+                    <input type="radio" name="finish" value="finish" id="finish">
+                </td>
+                <td class="delete">
+                    <input type="checkbox" name="delete" value="delete" id="delete">
+                </td>
+                <td>
+                    <button>実行</button>
+                </td>
+            </tr>
+            <tr>
+                <td class="date">2020-01-07</td>
+                <td class="project">ワークで難波神社へ初詣に行</td>
+                <td class="unfinish">
+                    <input type="radio" name="unfinish" value="unfinish">
+                </td>
+                <td class="finish">
+                    <input type="radio" name="finish_" value="finish" id="finish_02">
+                </td>
+                <td class="delete">
+                    <input type="checkbox" name="delete" value="delete" id="delete_02">
+                </td>
+                <td>
+                    <button>実行</button>
+                </td>
+            </tr>
+            <tr>
+                <td class="date">2020-02-03</td>
+                <td class="project">ITワーク・PHP開発者のためのBootstrap4入門</td>
+                <td class="unfinish">
+                    <input type="radio" name="unfinish" value="unfinish">
+                </td>
+                <td class="finish">
+                    <input type="radio" name="finish" value="finish" id="finish_03">
+                </td>
+                <td class="delete">
+                    <input type="checkbox" name="delete" value="delete" id="delete_03">
+                </td>
                 <td>
                     <button>実行</button>
                 </td>
