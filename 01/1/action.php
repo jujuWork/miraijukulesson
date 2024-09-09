e729a3bba31d579a43b522530d3340026fba2ec6
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-09 22:28:05 +0900

    090924

---
+++ 01/1/action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>練習問題</title>
+</head>
+<body>
+    <div class="container">
+        id: 10
+        name: 未来のかたち
+    </div>
+</body>
+</html>
