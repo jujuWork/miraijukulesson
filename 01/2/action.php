b32847eb12187d2157a15212b758a036e3eb39eb
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-10 21:53:09 +0900

    091024

--- 01/2/action.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$type = $_POST['10'];
-$name = $_POST['未来のかたち'];
-
-echo $type;
-echo $name;
--- phpExercises/01/1/action.php
+++ phpExercises/01/1/action.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="jp">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>練習問題</title>
-
-    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
 </head>
 <body>
-    <div class="container"></div>
+    ACTION PHP
 </body>
 </html>
