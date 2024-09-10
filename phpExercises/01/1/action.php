85e7e9f466bdea23678caa8f4284762de8e3b442
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-10 22:26:55 +0900

    091024

--- phpExercises/01/1/action.php
+++ phpExercises/01/1/action.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ACTION PHP
-</body>
-</html>
+<?php
+
+$type = $_GET["fruits"];
+$name = $_GET["apple"];
+
+print_r($type);
