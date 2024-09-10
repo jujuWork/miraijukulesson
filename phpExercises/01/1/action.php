ac0365cfefd835ba5392fd2f9f5d87b30b199ba1
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-10 22:38:03 +0900

    091024

--- phpExercises/01/1/action.php
+++ phpExercises/01/1/action.php
@@ -1,6 +1,9 @@
 <?php
 
-$type = $_GET["fruits"];
-$name = $_GET["apple"];
+// $type = $_GET["fruits"];
+// $name = $_GET["apple"];
 
-print_r($type);
+var_dump($_SERVER["REQUEST_METHOD"]);
+// if ($_SERVER["REQUEST_METHOD"]) {
+
+// }
