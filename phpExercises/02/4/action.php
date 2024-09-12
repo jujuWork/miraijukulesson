c0efdde602892676cb0c7f755c0289882de281d0
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-12 23:32:31 +0900

    091224

--- phpExercises/02/4/action.php
+++ phpExercises/02/4/action.php
@@ -4,6 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     $numberA = htmlspecialchars($_POST["numberA"]);
     $numberB = htmlspecialchars($_POST["numberB"]);
+    $result = ($numberA - $numberB);
 
-    echo "数字A - 数字Bは" . "<br>" . $numberA - $numberB . "<br>" . "です";
+    echo "数字A - 数字Bは" . "<br>" . $result . "<br>" . "です";
 }
