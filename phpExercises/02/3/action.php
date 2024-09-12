c8f0679c698c92fc4d2a9354ec79970bdd182b6a
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-12 22:52:50 +0900

    091224

--- phpExercises/02/3/action.php
+++ phpExercises/02/3/action.php
@@ -5,9 +5,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $numberA = $_POST["numberA"];
     $numberB = $_POST["numberB"];
 
-    echo "数字A+数字Bは";
-    echo "<br><br>";
-    echo $numberA + $numberB;
-    echo "<br><br>";
-    echo "です";
+    echo "数字A + 数字Bは" . "<br>" . $numberA + $numberB . "<br>" . "です";
 }
