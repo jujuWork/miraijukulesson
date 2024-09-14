e9d0f702be023f0d225fbd043e41aed029335fe8
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-14 21:17:21 +0900

    091424

---
+++ phpExercises/03/2/action.php
@@ -0,0 +1,18 @@
+<?php
+
+$numberA = (int)$_POST["numberA"];
+
+// if ($numberA >=100) {
+//     echo "100以上です!";
+// } else {
+//     echo "100未満です";
+// }
+
+switch ($numberA) {
+    case "$numberA <= 60":
+        echo ("your score is less that 60");
+            break;
+    case "$numberA 60 == 70":
+        echo ("your score is in 60 ~ 70");
+            break;
+}
