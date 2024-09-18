4a9132812529b32600b9ba1f39226b7d7b021436
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-18 10:15:19 +0900

    091824

--- phpExercises/03/3/action.php
+++ phpExercises/03/3/action.php
@@ -1,16 +1,38 @@
 <?php
 
-$japaneseScore = (int)$_POST["japaneseScore"];
-$mathScore = (int)$_POST["mathScore"];
+// $japaneseScore = (int)$_POST["japaneseScore"];
+// $mathScore = (int)$_POST["mathScore"];
 
-// switch ($japaneseScore. $mathScore) {
-//     case ($japaneseScore. $mathScore >= 80):
-//         echo "国語の点数が80点以上かつ算数の点数が80点以上 素晴らしいです!";
+// switch ($japaneseScore && $mathScore) {
+//     case ($japaneseScore >= 80 && $mathScore >= 80);
+//         echo "国語の点数が 80 点以上かつ算数の点数が 80 点以上素晴らしいです！";
 //             break;
+//     case ($japaneseScore >= 80 && !$mathScore <= 80);
+//         echo "国語の点数が 80 点以上、または、算数の点数が 80 点以上良しとしましょう！";
+//             break;
+//     case ($japaneseScore <= 80 && !$mathScore <= 80);
+//         echo "それ以外まあ、いいでしょう・・・";
+//             break;
+
+//     header("Location: /index.html");
 // }
 
-if ($japaneseScore. $mathScore <= 80) {
-    echo "国語の点数が80点以上かつ算数の点数が80点以上 素晴らしいです!";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $japaneseScore = htmlspecialchars((int)$_POST["japaneseScore"]);
+    $mathScore = htmlspecialchars((int)$_POST["mathScore"]);
+
+    if (empty($japaneseScore)) {
+        exit ();
+    }
+
+    if ($japaneseScore && $mathScore >= 80) {
+        echo "国語の点数が 80 点以上かつ算数の点数が 80 点以上素晴らしいです！";
+    } else if ($japaneseScore >= 80 && $mathScore <= 80) {
+        echo "国語の点数が 80 点以上、または、算数の点数が 80 点以上良しとしましょう！";
+    } else {
+        echo "それ以外まあ、いいでしょう・・・";
+    }
 } else {
-    echo "its alright!";
+    header("Location: index.html");
+    exit;
 }
