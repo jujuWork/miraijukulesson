cd63fbe90839608019b4ab9709ece7b81ef652dd
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-18 09:13:13 +0900

    091824

---
+++ phpExercises/03/3/action.php
@@ -0,0 +1,16 @@
+<?php
+
+$japaneseScore = (int)$_POST["japaneseScore"];
+$mathScore = (int)$_POST["mathScore"];
+
+// switch ($japaneseScore. $mathScore) {
+//     case ($japaneseScore. $mathScore >= 80):
+//         echo "国語の点数が80点以上かつ算数の点数が80点以上 素晴らしいです!";
+//             break;
+// }
+
+if ($japaneseScore. $mathScore <= 80) {
+    echo "国語の点数が80点以上かつ算数の点数が80点以上 素晴らしいです!";
+} else {
+    echo "its alright!";
+}
