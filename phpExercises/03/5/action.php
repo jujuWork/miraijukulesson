0ed6bb0fc96274b0afcdf716b432f59081aa3be0
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-18 22:57:09 +0900

    091824

--- phpExercises/03/5/action.php
+++ phpExercises/03/5/action.php
@@ -1,7 +1,11 @@
 <?php
 
-// $loopCount = (int)$_POST["loopCount"];
 
-for ($i = 1; $i <= $_POST["loopCount"]; $i++) {
-    echo "<li>" . $i . "回目</li>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $loopCount = (int)$_POST["loopCount"];
+
+    for ($i = 1; $i <= $_POST["loopCount"]; $i++) {
+        echo "<li>" . $i . "回目</li>";
+        }
+
 }
