76ad17851aab48e2f187b2c6c0cdd4bd84f26621
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-19 21:50:50 +0900

    091924

--- phpExercises/03/5/action.php
+++ phpExercises/03/5/action.php
@@ -1,11 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $loopCount = (int)$_POST["loopCount"];
-
-    for ($i = 1; $i <= $_POST["loopCount"]; $i++) {
-        echo "<li>" . $i . "回目</li>";
+    <p><?= $_POST["loopCount"] ?> 回ループします。</p>
+    <?php
+    $loopCount = $_POST["loopCount"];
+        for ($i = 1; $i <= $loopCount; $i++) {
+            echo "<li>" . $i . "回目</li>";
         }
 
-}
+    ?>
+</body>
+</html>
