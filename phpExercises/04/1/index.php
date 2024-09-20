f16201af18a19d3bf4b50889daf9546e9e147554
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-20 23:15:30 +0900

    092024

--- phpExercises/04/1/index.php
+++ phpExercises/04/1/index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <? echo "果物のリスト" ?>
+    <?php
+        
+        $array = ["みかん", "りんご", "メロン", "バナナ", "パイナップル"];
+            
+                foreach ($array as $array) {
+                    echo "<li>" . $array . "</li>";
+                }
+        ?>
+    
+</body>
+</html>
 
-$array = ["みかん", "りんご", "メロン", "バナナ", "パイナップル"];
-
-print_r($array);
