ecde9fc9001a00154b2833b231e986cb7b7977b8
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-20 23:36:44 +0900

    092024

---
+++ phpExercises/04/3/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+        <? echo "<table>". "<td>" . "メニュー" . "</td>" . "</table>" ?>
+    <?php
+        
+        $array = 
+        [
+            "Aランチ", "Bランチ", "Cランチ", "唐揚げ定食", "とんかつ定食", 
+            "エビフライ定食", "オムライス", "カレーライス", "ごはん大",
+            "ごはん小", "ビール", "烏龍茶"
+        ];
+            
+                foreach ($array as $array) {
+                    echo "<table>" . "<td>" . $array . "</td>" . "</table>";
+                }
+        ?>
+    
+</body>
+</html>
+
