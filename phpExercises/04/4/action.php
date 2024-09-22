c5dbe4b6f2d049c9f592b3f054287da9ff5734a6
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-22 22:05:53 +0900

    092224

---
+++ phpExercises/04/4/action.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <p><h3>お選びになったメニューはこちらです</h3></p>
+    <?php
+
+    foreach ($_POST['checkbox'] as $checkbox) {
+        echo "<table>" . $checkbox . ' ' . "</table>";
+    }
+
+    ?>
+    
+</body>
+</html>
+
