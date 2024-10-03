d50621a70e807ab1a051dc88d37427c659151152
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-03 23:26:39 +0900

    100324

---
+++ phpExercises/06/4/cart_add.php
@@ -0,0 +1,14 @@
+<?php
+
+// Starting Session
+session_start();
+
+// Session Regenerate ID
+session_regenerate_id();
+
+// Saving the posted valued as is
+$_SESSION['cart'][] = $_POST;
+
+// back to the index.php
+header('location ./index.php');
+exit;
