63cf1f3b8901102885c47f40286ec17ced2c9332
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-06 23:37:09 +0900

    100624

---
+++ phpExercises/07/1/Dog.php
@@ -0,0 +1,18 @@
+<?php
+
+class Dog 
+{
+        // Properties / Fields
+    public $dog = "hachi";
+
+        // Constructor
+    // public function __construct($dog)
+    // {
+    //     $this->dog = $dog;
+    // }
+}
+
+$breed = new Dog("Hachi");
+
+echo $breed->dog;
+
