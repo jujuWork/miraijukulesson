13eb5b1af5faa80db9e4de7b56abdcf6443e8014
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-15 00:13:01 +0900

    101424

--- phpExercises/07-1/6/index.php
+++ phpExercises/07-1/6/index.php
@@ -2,8 +2,10 @@
 
 require_once('coins.php');
 
-$coins = new coinCase();
-$coins->addCoins(500, 100, 50, 10, 5, 1);
-$coins->getCount();
-$coins->getAmount();
-$ccins->showProfile();
+$coin01 = 500;
+$coin02 = 100;
+$coin03 = 50;
+$coin04 = 10;
+$coin05 = 5;
+$coin06 = 1;
+
