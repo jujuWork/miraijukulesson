4ad526eb3ce54710e396a21428f354fdcb0e300a
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-16 22:56:53 +0900

    101624

--- phpExercises/07-1/6/CoinCase.php
+++ phpExercises/07-1/6/CoinCase.php
@@ -1,29 +1,22 @@
 <?php
+    // new Instance
+$coinCase = new CoinCase();
 
-class CoinCase 
-{
-    private $coin = 500;
-    private $count = 4;
+    // Looping the coins 10 times
+for ($i = 0; $i < 10; $i++) {
+    // enter the coin type and number of coins to add
+    $coinType = [500, 100, 50, 10, 5, 1][$i % 6]; // rotating the coins 6 times
+    $coinCount = $i + 1; // example to increase coin count each time
 
-    public function __construct($coin, $count)
-    {
-        $this->coin = $coin;
-        $this->count = $count;
-    }
-
-    public function amount()
-    {
-        return $this->coin . "Yen " . $this->count . " pcs";
-    }
-
-    public function count()
-    {
-        $this->count += 1;
-    }
-
-
+    $coinCase->addCoins($coinType, $coinCount); // Add coins
+    echo "Added $coinCount coins of $coinType yen. \n";
 }
 
+echo "Number of 500 yen coins: " . $coinCase->getCount(500) . "\n";
+echo "Number of 100 yen coins: " . $coinCase->getCount(100) . "\n";
+echo "Number of 50 yen coins: " . $coinCase->getCount(50) . "\n";
+echo "Number of 10 yen coins: " . $coinCase->getCount(10) . "\n";
+echo "Number of 5 yen coins: " . $coinCase->getCount(5) . "\n";
+echo "Number of 1 yen coins: " . $coinCase->getCount(1) . "\n";
 
-$coin01 = new Coin500 (500, 4);
-$coin01->amount();
+echo "Total amount of money: " . $coinCase->getAmount() . "yen.\n";
--- phpExercises/07-1/6/index.php
+++ phpExercises/07-1/6/index.php
@@ -2,13 +2,61 @@
 
 require_once ('CoinCase.php');
 
-class Coin500
+class CoinCase
 {
-    public $coin01 = 500;
+    private $coins_500 = 0;
+    private $coins_100 = 0;
+    private $coins_50 = 0;
+    private $coins_10 = 0;
+    private $coins_5 = 0;
+    private $coins_1 = 0;
 
-    public function amount()
+        // Adding coins
+    public function addCoins($type, $number)
     {
-        $this->coin01 += 1;
-        // $this->count += 1;
+        if ($type == 500) {
+            $this->coins_500 += $number;
+        } elseif ($type == 100){
+            $this->coins_100 += $number;
+        } elseif ($type == 50) {
+            $this->coins_50 += $number;
+        } elseif ($type == 10) {
+            $this->coins_10 += $number;
+        } elseif ($type == 5) {
+            $this->coins_5 += $number;
+        } elseif ($type == 1) {
+            $this->coins_1 += $number;
+        }
     }
+
+        // Method to get the count of a coin type
+    public function getCount($type)
+    {
+        if ($type == 500) {
+            return $this->coins_500;
+        } elseif ($type == 100) {
+            return $this->coins_100;
+        } elseif ($type == 50) {
+            return $this->coins_50;
+        } elseif ($type == 10) {
+            return $this->coins_10;
+        } elseif ($type == 5) {
+            return $this->coins_5;
+        } elseif ($type == 1) {
+            return $this->coins_1;
+        }
+        return 0; // if the given coins is invalid
+    }
+        
+    public function getAmount() {
+        $total = 0;
+        $total += $this->coins_500 * 500;
+        $total += $this->coins_100 * 100;
+        $total += $this->coins_50 * 50;
+        $total += $this->coins_10 * 10;
+        $total += $this->coins_5 * 5;
+        $total += $this->coins_1 * 1;
+        return $total;
+    }
+
 }
