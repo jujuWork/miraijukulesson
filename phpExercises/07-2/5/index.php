b8534e5ec976b25aa3fd5f1887022941f67669d4
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-16 23:25:54 +0900

    101624

---
+++ phpExercises/07-2/5/index.php
@@ -0,0 +1,76 @@
+<?php
+
+class Dog
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function showProfile()
+    {
+        return "名前は " . $this->name . " です." . "年齢は " . $this->age . "歳です。";
+    }
+
+    public function run()
+    {
+        echo "トコトコ";
+    }
+
+    public function speak()
+    {
+        echo "........";
+    }
+}
+
+class Cat
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function showProfile()
+    {
+        return "名前は " . $this->name . " です." . "年齢は " . $this->age . "歳です。";
+    }
+
+    public function sleep()
+    {
+        echo "スースー";
+    }
+
+    public function speak()
+    {
+        echo "........";
+    }
+}
+
+$dog01 = new Dog('はち', 5);
+echo $dog01->showProfile();
+echo "<br>";
+echo "<br>";
+echo $dog01->run(); 
+echo "<br>";
+echo "<br>";
+echo $dog01->speak();
+
+echo "<br>";
+echo "<br>";
+
+$cat01 = new Cat('たま', 3);
+echo $cat01->showProfile();
+echo "<br>";
+echo "<br>";
+echo $cat01->sleep();
+echo "<br>";
+echo "<br>";
+echo $cat01->speak();
