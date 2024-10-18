e3ffe678f02b0d468352361d09de64ab50767ab9
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-18 10:39:33 +0900

    101824

---
+++ phpExercises/07-3/2/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    // Create intance of each class
+$pc = new PersonalComputer();
+$tablet = new TabletPC();
+$smartphone = new SmartPhone();
+
+    // Show the type of each computer and demonstrate theri input, output
+$pc->showType();
+$pc->input();
+$pc->output();
+$pc->communication();
+echo "<br>";
+
+$tablet->showType();
+$tablet->input();
+$table->output();
+$tablet->communication();
+echo "<br>";
+
+$smartphone->showType();
+$smartphone->input();
+$smartphone->output();
+$smartphone->communication();
