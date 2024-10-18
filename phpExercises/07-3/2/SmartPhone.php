7d62919c8a739869fdec51edcf08a468a636b81a
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-18 22:56:44 +0900

    101824

--- phpExercises/07-3/2/SmartPhone.php
+++ phpExercises/07-3/2/SmartPhone.php
@@ -6,14 +6,16 @@ class SmartPhone extends Computer
 {
     public function __construct()
     {
-        parent::__construct("スマートフォン");
+        parent::__construct("スマートフォン"); // linking __contruct from Computer.php __contruct
     }
 
+        // Linking to the absstract method in Computer.ph
     public function input()
     {
         echo "タッチパネルディスプレイをタップして操作 <br>";
     }
 
+        // Linking to the absstract method in Computer.ph
     public function output()
     {
         echo "タッチパネルディスプレイに出力 <br>";
--- phpExercises/07-3/2/TabletPC.php
+++ phpExercises/07-3/2/TabletPC.php
@@ -6,14 +6,16 @@ class TabletPC extends Computer
 {
     public function __construct()
     {
-        parent::__construct("タブレットPC"); // Set the type
+        parent::__construct("タブレットPC"); // linking __contruct from Computer.php __contruct
     }
 
+        // Linking to the absstract method in Computer.php
     public function input()
     {
         echo "タッチパネルディスプレイをタップして操作 <br>";
     }
 
+        // Linking to the absstract method in Computer.php
     public function output()
     {
         echo "タッチパネルディスプレイに出力 <br>";
