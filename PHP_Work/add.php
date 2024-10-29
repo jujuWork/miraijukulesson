83c89767d285df7653049d51dd43edc02a6b3a6f
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-29 23:26:11 +0900

    102924

--- PHP_Work/add.php
+++ PHP_Work/add.php
@@ -1,19 +1,23 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=todolistdatabase;charset=utf8;";
-$dbusername = 'root';
-$dbpassword = 'root';
+// $dsn = "mysql:host=localhost;dbname=todolistdatabase;charset=utf8;";
+$host = '127.0.0.1';
+$port = '8889';
+$dbname = 'todolistdatabase';
+$username = 'root';
+$password = 'root';
 
 try {
-    $pdo = new PDO($dsn, $dbusername, $dbpassword); // MOST IMPORTANT FOR CONNECTING THE DATABASE
-        // ERROR HANDLERS
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password); // MOST IMPORTANT FOR CONNECTING THE DATABASE
+    //     // ERROR HANDLERS
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+    
+    $sql = "INSERT INTO todo_items (expiration_date, todo_item) VALUES ('" . $_POST['expiration_date'] . "', '" . $_POST['todo_item'] . "')";
+
+    $pdo->exec($sql);
+
+    header("Location: index.php");
+
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
-$expiration_date = $_POST['expiration_date'];
-$todo_item = $_POST['todo_item'];
-
-$sql = "INSERT INTO todo_items (expiration_date, todo_item) VALUES ('$expiration_date', '$todo_item')";
-
