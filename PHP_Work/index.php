19a3a3554f92adf5becf11afbd32ab85c73ba293
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-10-30 22:15:18 +0900

    103024

--- PHP_Work/index.php
+++ PHP_Work/index.php
@@ -1,3 +1,27 @@
+<?php
+    // DATABASE CONNECTION DETAILS
+$host = '127.0.0.1';
+$port = '8889';
+$dbname = 'todolistdatabase';
+$usernamme = 'root';
+$password = 'root';
+
+try {
+        // Connect to the database
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // RETRIEVE ALL DATA
+    $sql = "SELECT expirationi_date, todo_item FROM todo_items";
+    $stmt = $pdo->query($sql);
+
+        // FETCH ALL RESULT
+    $todos = $stmt->fetch();
+
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,21 +30,33 @@
     <title>Todo Form</title>
 
     <link rel="stylesheet" href="styles.css">
+
+    <!-- Calendar POP UP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 </head>
 <body>
     <main>
-        <div id="form_header">
-            <label for="Todo List">TODOリスト</label>
-        </div>
-        <div id="form_input">
+        <header>
+            <h1 class="header_text">TODO List</h1>
+        </header>
+
+        <section>
             <form action="add.php" method="post">
-                <input type="date" name="expiration_date" id="expiration_date" value="<?php echo date('Y/m/d', time() + 9 *3600); ?>" class="input_date">
+                <div class="listContainer">
+                    <input type="text" name="expiration_date" id="expiration_date" value="<?php echo date('Y/m/d', time() + 9 *3600); ?>">
+                    <input type="text" name="todo_item" id="todo_item" placeholder="TODO リストを⼊⼒してください">
 
-                <input type="text" name="todo_item" class="input_text_list" placeholder="TODO List ...">
-
-                <button type="submit">Add</button>
+                    <button>Add</button>
+                </div>
             </form>
-        </div>
+
+            <script>
+                flatpickr("#expiration_date", {
+                    dateFormat: "Y/m/d",
+                });
+            </script>
+        </section>
     </main>
 </body>
 </html>
