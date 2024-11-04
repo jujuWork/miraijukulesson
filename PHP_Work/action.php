96e26e1486d2e0f38614ee248471b536583ab360
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-11-04 23:52:30 +0900

    110424

--- PHP_Work/action.php
+++ PHP_Work/action.php
@@ -16,8 +16,6 @@ try {
     $status = $_POST['status']; // ? (int)$_POST['status'] : 0;        // Get Status or set to 0
     $delete = isset($_POST['delete']) ? 1 : 0;      // True if the delete checkbox was checked
 
-    var_dump($id, $status, $delete);
-    exit;
             // IF delete is checked, delete the item
         if ($delete) {
             $sql = "DELETE FROM todo_items WHERE id = :id";
