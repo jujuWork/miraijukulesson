359d5ce965fa125b27611b054ed1e5eda0c199cf
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-11-12 21:33:24 +0900

    111224

--- todo_list/csvUpdate.php
+++ todo_list/csvUpdate.php
@@ -1,29 +1,47 @@
 <?php
 
-$dsn = 'mysql:host=127.0.0.1;dbname=todolistdatabase;charset=utf8';
+$host = '127.0.0.1';
+$port = '8889';
+$dbname = 'todolistdatabase';
 $username = 'root';
 $password = 'root';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['files'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
     try {
             // Establish a database connection
-        $pdo = new PDO($dsn, $username, $password);
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // Open uploaded CSV file
         $file = $_FILES['file']['tmp_name'];
         $fp = fopen($file, 'r');
 
+        $firstrow = true;
             // Read each lin in the CSV file
         while (($row = fgetcsv($fp)) !== false) {
+                // Skip header row if there is one
+            if ($firstrow) {
+                $firstrow = false;
+                continue;
+            }
+
                 // Convert the encoding of each row from Shift-JIS to UTF-8
             $row = array_map(fn($field) => mb_convert_encoding($field, 'UTF-8', 'SJIS-win'), $row);
+
+                // Ensure we have exactly number of columns
+            if (count($row) < 4) {
+                throw new Exception("CSV row format is incorrect");
+            }
 
                 // Extract the values from each row
             list($id, $expiration_date, $todo_item, $is_completed) = $row;
 
+            if (!is_numeric($id)) {
+                throw new Exception("Invalid ID format: $id is no numeric.");
+            }
+
                 // Updating record with macting ID
-            $stmt = $pdo->prepare("UPDATE todo_items SET due_date = ?, todo_item = ?, completed = ? Where id = ?");
+            $stmt = $pdo->prepare("UPDATE todo_items SET expiration_date = ?, todo_item = ?, is_completed = ? Where id = ?");
             $stmt->execute([$expiration_date, $todo_item, $is_completed, $id]);
         }
 
