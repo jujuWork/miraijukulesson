0f62e263a43e9b0b1baa0d67b7d08e05f7d37ba9
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-11-19 23:34:52 +0900

    111924

--- todo_list/csvUpdate.php
+++ todo_list/csvUpdate.php
@@ -32,7 +32,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
                 $firstrow = false;
                 continue;
             }
-    
                 // Convert the encoding of each row from Shift-JIS to UTF-8
             $todo = array_map(fn($field) => mb_convert_encoding($field, 'UTF-8', 'SJIS-win'), $todo);
 
@@ -40,6 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
             if (count($todo) !== 5) {
                 // throw new Exception("CSV row format is incorrect");
                 echo "Skipping invalid row: " . implode(", ", $todo) . "<br>";
+                continue;
             }
 
                 // Extract the values from each row
@@ -63,15 +63,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
                                     todo_item = :todo_item, 
                                     is_completed = :is_completed,
                                     status = :status
-                                    Where id = :id");
-            $stmt->bindParam(':expiration_date', $expiration_date, \PDO::PARAM_STR);
-            $stmt->bindParam(':todo_item', $todo_item, \PDO::PARAM_STR);
-            $stmt->bindParam(':is_completed', $is_completed, \PDO::PARAM_INT);
-            $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
-            $stmt->bindParam(':status', $status, \PDO::PARAM_INT);
+                                    WHERE id = :id");
+            $stmt->bindParam(':expiration_date', $expiration_date, PDO::PARAM_STR);
+            $stmt->bindParam(':todo_item', $todo_item, PDO::PARAM_STR);
+            $stmt->bindParam(':is_completed', $is_completed, PDO::PARAM_INT);
+            $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
             $stmt->execute();
 
-            echo "Updated row with ID: $id<br>";
+            echo "Rows Updated: " . $stmt->rowCount() . "<br>";
 
         }
 
