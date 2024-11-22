ea1d05d5f0ce494a6f61a85cc64a4a62a639c2b4
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-11-22 23:07:05 +0900

    112224

---
+++ login/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="loginstyles.css">
+</head>
+<body>
+    
+    <main>
+        <h3>Login</h3>
+        
+        <div class="login">
+            <form action="action.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <button type="submit" class="logBtn">Login</button>
+            </form>
+        </div>
+    </main>
+
+</body>
+</html>
